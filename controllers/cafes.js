384f2fe45f76a7a7189d383770e092e15bc86987
EleahBurman <eleahjburman@gmail.com>
2023-04-23 17:47:29 -0400

    Function update in place to update cafes via put

--- controllers/cafes.js
+++ controllers/cafes.js
@@ -1,4 +1,4 @@
-import {Cafe} from "../models/cafe.js"
+import { Cafe } from "../models/cafe.js"
 function index(req, res) {
   Cafe.find({})
   .then(cafes => {
@@ -19,7 +19,38 @@ function newCafe(req, res) {
   })
 }
 
+function create(){
+  //if an empty string is added to create a cafe, delete that string
+  for (let key in req.body) {
+    if (req.body[key] === '') delete req.body[key]
+  }
+  //
+  Cafe.create(req.body)
+  .then(cafe =>{
+    res.redirect('/cafes/${cafe._id}`')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/cafes/new')
+  })
+}
+
+function update(){
+  for (let key in req.body) {
+    if (req.body[key] === '') delete req.body[key]
+  }
+  Cafe.findByIdAndUpdate(req.params.cafeId, req.body, {new: true})
+  .then(cafe => {
+    res.redirect(`/cafes/${cafe._id}`)
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/cafes')
+  })
+}
 export {
   index,
   newCafe as new,
+  create,
+  update
 }
--- routes/cafes.js
+++ routes/cafes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express'
 import * as cafesCtrl from '../controllers/cafes.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router ()
 // GET localhost:3000/cafes
 router.get('/', cafesCtrl.index)
 // GET localhost:3000/cafes/new
 router.get('/new', cafesCtrl.new)
-
+// POST localhost:3000/cafes
+router.post('/', isLoggedIn, cafesCtrl.create)
+// PUT localhost:3000/cafes/:cafeId
+router.put('/:cafeId', cafesCtrl.update)
 export { router }
